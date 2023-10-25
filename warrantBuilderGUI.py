45f71294d6715158a961f775d20e18ddcc22b22e
Whee30 <99293401+Whee30@users.noreply.github.com>
2023-10-24 18:00:08 -0700

    Add files via upload

--- warrantBuilderGUI.py
+++ warrantBuilderGUI.py
@@ -1,13 +1,15 @@
 import os
-impor*t PySimpleGUI as sg
+import PySimpleGUI as sg
+from textwrap import wrap
 from pathlib import Path
 from docxtpl import DocxTemplate
+
 # YouTube tutorial for these functions
 # https://www.youtube.com/watch?v=fziZXbeaegc
 
 
 # Where the source template file lives
-templatePath = Path(__file__).parent / "template.docx"
+templatePath = Path(__file__).parent / "WarrantSkeleton.docx"
 # Output file variable
 docOut = DocxTemplate(templatePath)
 
@@ -16,60 +18,62 @@ resSrc = open('./sources/residence.txt').read()
 vehSrc = open('./sources/vehicle.txt').read()
 socSrc = open('./sources/social.txt').read()
 
+s = (
+    "Cellphones",
+    "Residences",
+    "Vehicles"
+    )
 
-# User input variables
-# userName = input("Please enter your name:")
-# caseNum = input("Please enter your case number:")
+# Tuple containing reasons, checkboxes will call these
+r = (
+	"Were stolen or embezzled",
+	"Were used as a means for committing a public offense.",
+	"Is being possessed with the intent to use it as a means of committing a public offense",
+	"Are in the possession of to whom it was delivered for the purpose of concealing it or preventing it from being discovered",
+	"Consists of any item or constitutes any evidence which tends to show that a public offense has been committed, or tends to show that a particular person committed the public offense",
+	"The person sought is the subject of an outstanding arrest warrant which offense occurred on or about the day of , 20 in the County of Pima, State of Arizona",
+	"In the daytime (excluding the time period between 10:00 p.m and 6:30 a.m)",
+	"In the nighttime"
+	)
+
+ranks = ["Ofc.","Det.","Sgt."]
 
 layout = [
-    [sg.Text("Name"), sg.Input(key="NAME")],
-    [sg.Text("Case Number"), sg.Input(key="CASENUM_FIELD")],
+    [sg.Text('Rank:'),
+    sg.Combo(ranks, key='RANK'), 
+    sg.Text('Name:'), 
+    sg.Input(key="NAME", size=(10,1)), 
+    sg.Text('Badge #:'), 
+    sg.Input(key="BADGE", size=(10,1)),
+    sg.Text('Case Number:', size=(12,1)), 
+    sg.Input(key="CASENUM", size=(20,1))],
+    [sg.Text('Suspect Name:', size=(12,1)), 
+    sg.Input(key="SUSPECT", size=(20,1))],
+    [sg.Listbox(s, size=(20, 4), key='s1', enable_events=True),
+    sg.Checkbox("Residence", key='s2'),
+    sg.Checkbox("Social", key='s3')],
+    [sg.Checkbox(text=f"{r[0]}", key='r[0]')],
+    [sg.Checkbox(text=f"{r[1]}", key='r[1]')],
+	[sg.Checkbox(text=f"{r[2]}", key='r[2]')],
+	[sg.Checkbox(text=f"{r[3]}", key='r[3]')],
+	[sg.Checkbox(text=f"{r[4]}", key='r[4]')],
+	[sg.Checkbox(text=f"{r[5]}", key='r[5]')],
     [sg.Button("Generate Warrant"), sg.Exit()],
-    [sg.Checkbox("Vehicle", key='c1')],
-    [sg.Checkbox("Residence", key='c2')],
-    [sg.Checkbox("Social", key='c3')],
+
     
 ]
     
-window = sg.Window("Warrant Builder V0.1", layout)
+window = sg.Window("Warrant Builder V0.1", layout, size=(800, 600))
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Exit":
         break
     if event == "Generate Warrant":
-        value["NAME"]
-        docOut.render
-        print(event, values)
+        docOut.render(values)
+        output_path = Path(__file__).parent / f"output/{values['CASENUM']}-search warrant.docx"
+        docOut.save(output_path)
+        sg.popup("Warrant built, don't forget to proofread!", f"File has been saved to: {output_path}")
+        break
         
 window.close()
-
-# Create a variable that will name the output file
-#fullPath = caseNum + " search warrant.docx"
-
-# Tuple containing reasons, checkboxes will call these
-#r = (
-#	"Were stolen or embezzled",
-#	"Were used as a means for committing a public offense.",
-#	"Is being possessed with the intent to use it as a means of committing a public offense",
-#	"Are in the possession of to whom it was delivered for the purpose of concealing it or preventing it from being discovered",
-#	"Consists of any item or constitutes any evidence which tends to show that a public offense has been committed, or tends to show that a particular person committed the public offense",
-#	"The person sought is the subject of an outstanding arrest warrant which offense occurred on or about the day of , 20 in the County of Pima, State of Arizona",
-#	"In the daytime (excluding the time period between 10:00 p.m and 6:30 a.m)",
-#	"In the nighttime"
-#	)
-	
-
-# Can only render once! Only the last render call will happen and the previous calls will be wiped out
-#docOut.render({
-#    "NAME": userName,
-#    "RESIDENCE": resSrc,
-#    "VEHICLE": vehSrc,
-#    "SOCIAL": socSrc,
-#    "CASENUMBER": caseNum
-#})
-
-# Save the rendered changes to the file based on the case number entered.
-#docOut.save(Path(__file__).parent / fullPath)
-
-#input('Press ENTER to exit')
