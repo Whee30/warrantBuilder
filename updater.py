cbe21503ce3aeaac5a8398f7b0709e3207ea1874
Whee30 <forrest.cook@gmail.com>
2025-08-20 17:13:56 -0700

    modified:   updater.py

--- updater.py
+++ updater.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import time
 import requests
 
 
@@ -7,10 +8,13 @@ no_cache_headers = {
     'Cache-Control': 'no-cache'
 }
 
-remote_hash_list = "https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/sources/hash_list.json"
+remote_hash_list = f"https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/sources/hash_list.json?nocache={int(time.time())}"
+remote_version_list = 
 
 response = requests.get(remote_hash_list, headers=no_cache_headers)
 hash_references = response.json()
+
+
 
 # The files needing hash validation
 remote_files = {
@@ -19,14 +23,35 @@ remote_files = {
     "template": "https://github.com/Whee30/warrantBuilder/raw/refs/heads/main/sources/skeleton.docx"
 }
 
-for k, v in hash_references.items():
+local_files = {
+    "program":"./dist/warrantBuilder.exe",
+    "verbiage":"./sources/cv_sources.json",
+    "template":"./sources/skeleton.docx"
+}
+
+def compare_version(k):
+
+
+def compare_hashes(k):
     hash_to_compare = hash_references[k]
     remote_sha256_hash = hashlib.sha256()
     response = requests.get(remote_files[k], headers=no_cache_headers)
     remote_sha256_hash.update(response.content)
     if remote_sha256_hash.hexdigest() == hash_to_compare:
-        print("The hashes match")
+        replace_file(k)
     elif remote_sha256_hash.hexdigest() != hash_to_compare:
         print("The hashes don't match!")
         print(f"{k} calculated: {remote_sha256_hash.hexdigest()}")
         print(f"{k} Stored:     {hash_to_compare}")
+
+def replace_file(new_file):
+    print(f"{local_files[new_file]} would be replaced")
+    file_response = requests.get(remote_files[new_file], headers=no_cache_headers)
+    with open(local_files[new_file], 'wb') as file:
+        file.write(file_response.content)
+    # update the version number now
+    local_version_data[file] = remote_data[target]
+    with open('local_version.json', 'w') as file:
+        json.dump(local_version_data, file, indent=4)
+    print(f"{file} was updated.")
+
