efbb6dbabb3f9ef0acd7a964b552f9d202119418
Whee30 <99293401+Whee30@users.noreply.github.com>
2025-08-21 21:18:06 -0700

    modified:   testing_ground/updater.py

--- testing_ground/updater.py
+++ testing_ground/updater.py
@@ -1,18 +1,12 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QListView, QFormLayout, QMessageBox, QSizePolicy, QCheckBox, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QTabWidget, QLineEdit, QComboBox, QPushButton, QLabel, QTextEdit, QFrame, QScrollArea, QDateEdit
-from PyQt6.QtCore import Qt, QDate, QDir, QModelIndex, QEvent
-from PyQt6.QtGui import QFileSystemModel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QTextEdit
+from PyQt6.QtCore import Qt, QTimer
 import os
-from datetime import datetime
-import json
-import glob
 #import pyi_splash
 import hashlib
 import json
 import time
 import requests
-
-local_versions = {}
 
 required_items = {
     "output":"./output",
@@ -23,40 +17,30 @@ required_items = {
     "verbiage":"./sources/cv_sources.json",
     "template":"./sources/skeleton.docx",
     "settings":"./sources/settings.json",
-    "local_versions":"./sources/local_version.json"
 }
-
-#local_versions = {}
 
 no_cache_headers = {
     'Cache-Control': 'no-cache'
 }
 
 remote_hash_list = f"https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/sources/hash_list.json?nocache={int(time.time())}"
-remote_version_list =f"https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/sources/remote_version.json?nocache={int(time.time())}"
 
 h_response = requests.get(remote_hash_list, headers=no_cache_headers)
 hash_references = h_response.json()
-
-v_response = requests.get(remote_version_list, headers=no_cache_headers)
-remote_versions = v_response.json()
-
 
 # The files needing hash validation
 remote_files = {
     "program":"https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/warrantBuilder.py",
     "verbiage":"https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/sources/cv_sources.json",
     "template":"https://github.com/Whee30/warrantBuilder/raw/refs/heads/main/sources/skeleton.docx",
-    "settings":"https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/sources/settings.json",
-    "local_versions":"https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/sources/local_version.json"
+    "settings":"https://raw.githubusercontent.com/Whee30/warrantBuilder/refs/heads/main/sources/settings.json"
 }
 
 local_files = {
     "program":"./warrantBuilder.py",
     "verbiage":"./sources/cv_sources.json",
     "template":"./sources/skeleton.docx",
-    "settings":"./sources/settings.json",
-    "local_versions":"./sources/local_version.json"
+    "settings":"./sources/settings.json"
 }
 
 class MainWindow(QMainWindow):
@@ -71,30 +55,14 @@ class MainWindow(QMainWindow):
         # ESTABLISH WIDGETS #
         #####################
 
-        main_label = QLabel("Warrant Builder Update Utility 0.1")
+        self.main_label = QLabel("Warrant Builder Update Utility")
 
-        self.current_program = QLineEdit()
-        self.current_program.setReadOnly(True)
-
-        self.current_verbiage = QLineEdit()
-        self.current_verbiage.setReadOnly(True)
-
-        self.current_template = QLineEdit()
-        self.current_template.setReadOnly(True)
-
-        self.remote_program = QLineEdit()
-        self.remote_program.setReadOnly(True)
-
-        self.remote_verbiage = QLineEdit()
-        self.remote_verbiage.setReadOnly(True)
-
-        self.remote_template = QLineEdit()
-        self.remote_template.setReadOnly(True)
+        self.status_report = QTextEdit()
 
         self.submit_button = QPushButton()
         self.submit_button.setText("Upgrade!")
         self.submit_button.setFixedWidth(100)
-        self.submit_button.clicked.connect(lambda: run_update())
+        self.submit_button.clicked.connect(lambda: self.run_update())
 
         quit_button = QPushButton()
         quit_button.setText("Quit")
@@ -110,21 +78,6 @@ class MainWindow(QMainWindow):
         self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
         self.main.setLayout(self.main_layout)
 
-        self.content = QWidget()
-        self.content_layout = QHBoxLayout()
-        self.content_layout.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        self.content.setLayout(self.content_layout)
-
-        self.left_side = QWidget()
-        self.left_side_layout = QVBoxLayout()
-        self.left_side_layout.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        self.left_side.setLayout(self.left_side_layout)
-
-        self.right_side = QWidget()
-        self.right_side_layout = QVBoxLayout()
-        self.right_side_layout.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        self.right_side.setLayout(self.right_side_layout)
-
         self.button_row = QWidget()
         self.button_row_layout = QHBoxLayout()
         self.button_row_layout.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
@@ -135,20 +88,11 @@ class MainWindow(QMainWindow):
         # Add widgets to main #
         #######################
 
-        self.main_layout.addWidget(self.content)
+        self.main_layout.addWidget(self.main_label)
 
-        self.content_layout.addWidget(self.left_side)
-        self.content_layout.addWidget(self.right_side)
+        self.main_layout.addWidget(self.status_report)
 
         self.main_layout.addWidget(self.button_row)
-
-        self.left_side_layout.addWidget(self.current_program)
-        self.left_side_layout.addWidget(self.current_verbiage)
-        self.left_side_layout.addWidget(self.current_template)
-
-        self.right_side_layout.addWidget(self.remote_program)
-        self.right_side_layout.addWidget(self.remote_verbiage)
-        self.right_side_layout.addWidget(self.remote_template)
 
         self.button_row_layout.addWidget(self.submit_button)
         self.button_row_layout.addWidget(quit_button)
@@ -159,97 +103,65 @@ class MainWindow(QMainWindow):
         # Establish Functions #
         #######################
 
-        #def compare_version(k):
-        #    if local_versions[k] >= remote_versions[k]:
-        #        print("The versions are equal or the local version is newer")
-        #    elif local_versions[k] < remote_versions[k]:
-        #        print("The remote version is newer")
-        #        compare_hashes(k)
-        #    #print(f"Local: {local_versions[k]} - Remote: {remote_versions[k]}")
+    def compare_hashes(self, k):
+        hash_to_compare = hash_references[k]
+        remote_sha256_hash = hashlib.sha256()
+        response = requests.get(remote_files[k], headers=no_cache_headers)
+        remote_sha256_hash.update(response.content)
+        if remote_sha256_hash.hexdigest() == hash_to_compare:
+            self.replace_file(k)
+            print(f"The {k} file was updated.")
+        elif remote_sha256_hash.hexdigest() != hash_to_compare:
+            print(f"The hashes don't match! Something's wonky in dolphin-town. The {k} file was not replaced.")
+            print(f"{k} calculated: {remote_sha256_hash.hexdigest()}")
+            print(f"{k} Stored:     {hash_to_compare}")
+            self.status_report.append(f"Something went wrong with the {k} file, it was not updated.")
 
-        def compare_hashes(k):
+    def replace_file(self, k):
+        file_response = requests.get(remote_files[k], headers=no_cache_headers)
+        with open(local_files[k], 'wb') as file:
+            file.write(file_response.content)
+
+    def run_update(self):
+        self.status_report.append("Comparing file versions...")
+        for k, v in local_files.items():
             hash_to_compare = hash_references[k]
-            remote_sha256_hash = hashlib.sha256()
-            response = requests.get(remote_files[k], headers=no_cache_headers)
-            remote_sha256_hash.update(response.content)
-            if remote_sha256_hash.hexdigest() == hash_to_compare:
-                replace_file(k)
-                print(f"The calculated hash and the comparison hash for {k} matched, it was replaced")
-            elif remote_sha256_hash.hexdigest() != hash_to_compare:
-                print(f"The hashes don't match! The {k} file was not replaced.")
-                print(f"{k} calculated: {remote_sha256_hash.hexdigest()}")
-                print(f"{k} Stored:     {hash_to_compare}")
+            local_file_hash = hashlib.sha256()
+            with open(v, "rb") as file:
+                local_file_hash.update(file.read())
+            if local_file_hash.hexdigest() != hash_to_compare:
+                self.compare_hashes(k)
+        self.status_report.append("All files are up to date!")
 
-        def replace_file(k):
-            file_response = requests.get(remote_files[k], headers=no_cache_headers)
-            with open(local_files[k], 'wb') as file:
-                file.write(file_response.content)
-
-        def run_update():
-            print("run update just ran")
-            for k, v in local_versions.items():
-                if v < remote_versions[k]:
-                    compare_hashes(k)
-
-        def get_missing_files(k):
-            hash_to_compare = hash_references[k]
-            remote_sha256_hash = hashlib.sha256()
-            response = requests.get(remote_files[k], headers=no_cache_headers)
-            remote_sha256_hash.update(response.content)
-            if remote_sha256_hash.hexdigest() == hash_to_compare:
-                file_response = requests.get(remote_files[k], headers=no_cache_headers)
-                with open(local_files[k], 'wb') as file:
-                    file.write(file_response.content)
-                print(f"The calculated hash and the comparison hash for {k} matched, it was replaced")
-            elif remote_sha256_hash.hexdigest() != hash_to_compare:
-                print(f"The hashes don't match! The {k} file was not replaced.")
-                print(f"{k} calculated: {remote_sha256_hash.hexdigest()}")
-                print(f"{k} Stored:     {hash_to_compare}")
-
-        def initial_processing():
-            # Check for required directories and files        
-            if os.path.exists('./output') == False:
-                os.mkdir('./output')
-            if os.path.exists('./sources') == False:
-                os.mkdir("./sources")
-            if os.path.exists('./sources/previousWarrants') == False:
-                os.mkdir('./sources/previousWarrants')        
-            if os.path.exists('./sources/TandE.txt') == False:
-                with open("./sources/TandE.txt", "w") as file:
-                    file.write("This is where you include your relevant experience")
-            for k, v in required_items.items():
-                if os.path.exists(v) == False:
-                    get_missing_files(k)
-
-            print("The things all exist.")
-            
-            local_version_list = "./sources/local_version.json"
-
-            # Load local version numbers
-            with open(local_version_list, 'r') as file:
-                local_versions = json.load(file)
-
-            #local_versions[k] = remote_versions[k]
-            #with open(local_version_list, 'w') as file:
-            #    json.dump(local_versions, file, indent=4)
-            
-            self.current_program.setText(f"Current program version: {str(local_versions['program'])}")
-            self.current_verbiage.setText(f"Current verbiage version: {str(local_versions['verbiage'])}")
-            self.current_template.setText(f"Current template version: {str(local_versions['template'])}")
-            self.remote_program.setText(f"Current program version: {str(remote_versions['program'])}")
-            self.remote_verbiage.setText(f"Current verbiage version: {str(remote_versions['verbiage'])}")
-            self.remote_template.setText(f"Current template version: {str(remote_versions['template'])}")
-
-            
-
-
-
-
-        initial_processing()
+    def initial_processing(self):
+        self.status_report.append("Checking for all required files...")
+        # Check for required directories and files        
+        if os.path.exists('./output') == False:
+            os.mkdir('./output')
+            self.status_report.append("Output directory missing, directory was created.")
+        if os.path.exists('./sources') == False:
+            os.mkdir("./sources")
+            self.status_report.append("sources directory missing, directory was created.")
+        if os.path.exists('./sources/previousWarrants') == False:
+            os.mkdir('./sources/previousWarrants')
+            self.status_report.append("Previous warrants directory missing, directory was created.")      
+        if os.path.exists('./sources/TandE.txt') == False:
+            with open("./sources/TandE.txt", "w") as file:
+                file.write("This is where you include your relevant experience")
+            self.status_report.append("Training file was missing, file was created.")
+        for k, v in required_items.items():
+            if os.path.exists(v) == False:
+                self.compare_hashes(k)
+                self.status_report.append(f"{k} file was missing, file was downloaded.")
+        self.status_report.append("All files are present.")
+        
 app = QApplication(sys.argv)
 
 #pyi_splash.close()
 window = MainWindow()
 window.show()
 app.setStyle('Fusion')
+
+QTimer.singleShot(0, window.initial_processing)
+
 app.exec()
